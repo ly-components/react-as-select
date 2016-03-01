3ec7f27f7324d1db553d31d6377aed0b464382cf
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2016-03-01 23:51:21 +0800

    :smile:

--- test/index_spec.jsx
+++ test/index_spec.jsx
@@ -49,4 +49,18 @@ describe('test', function() {
     TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithClass(cp, 'select-body'));
     $(ReactDOM.findDOMNode(cp)).find('.select-list').css('display').should.be.eql('block');
   });
+  it('should hide options when click on document', function() {
+    let cp = ReactDOM.render(
+      <Select>
+        <Option value="html">HTML</Option>
+        <Option disabled value="javascript">JavaScript</Option>
+        <Option value="css">CSS</Option>
+      </Select>
+      , container);
+    cp = TestUtils.findRenderedComponentWithType(cp, Select);
+    TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithClass(cp, 'select-body'));
+    $(ReactDOM.findDOMNode(cp)).find('.select-list').length.should.eql(1);
+    $(document.body).trigger('click');
+    $(ReactDOM.findDOMNode(cp)).find('.select-list').length.should.eql(0);
+  });
 });
